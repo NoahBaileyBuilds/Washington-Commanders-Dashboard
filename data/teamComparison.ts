612383e5703948db810bbdd3e033985bbe4517e9
Noah Bailey <sharkboy.ndb@gmail.com>
2026-05-26 00:04:45 -0400

    Restore teamComparison data file

---
+++ data/teamComparison.ts
@@ -0,0 +1,19 @@
+const teamComparison = [
+	{
+		category: "Offense",
+		commanders: 80,
+		nfcEastAvg: 72
+	},
+	{
+		category: "Defense",
+		commanders: 68,
+		nfcEastAvg: 70
+	},
+	{
+		category: "Special Teams",
+		commanders: 74,
+		nfcEastAvg: 69
+	}
+];
+
+export default teamComparison;
